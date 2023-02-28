3d5baa5df351b4db6145cad66ea5594f61568d76
Miguel Faria <mattar1704miguel@gmail.com>
2023-02-28 11:08:39 -0300

    feat: add classes: Board And Piece

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,12 +3,8 @@ package boardgame;
 public class Position {
 	private int row;
 	private int column;
-	
-	public Position() {
-	}
 
 	public Position(int row, int column) {
-		super();
 		this.row = row;
 		this.column = column;
 	}
