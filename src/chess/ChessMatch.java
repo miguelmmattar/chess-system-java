0eb4538899b95d9f399c6f58a77114388d764050
Miguel Faria <mattar1704miguel@gmail.com>
2023-03-02 11:58:30 -0300

    feat: add possible moves for Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,7 +29,9 @@ public class ChessMatch {
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
+		
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		
 		Piece capturedPiece = makeMove(source, target);
 		
@@ -52,6 +54,11 @@ public class ChessMatch {
 		
 		if(!board.piece(position).isThereAnyPossibleMoves())
 			throw new ChessException("There is no possible move for chosen piece!");
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target))
+			throw new ChessException("The chosen piece cannot move to given target!");
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
