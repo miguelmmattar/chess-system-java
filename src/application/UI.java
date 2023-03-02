ca86b0cabf99d1599935e756afa207416435ad74
Miguel Faria <mattar1704miguel@gmail.com>
2023-03-02 16:28:25 -0300

    feat: add switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -43,6 +44,14 @@ public class UI {
 			throw new InputMismatchException("Error reading ChessPosition! Valid values are from a0 to h8.");
 		}
 		
+	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.printf("Waiting player " + chessMatch.getCurrentPlayer() +"...");
+		System.out.println();
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
